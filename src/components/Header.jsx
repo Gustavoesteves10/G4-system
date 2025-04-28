35110eff3013737f58ab909d98f5db2586083fbf
Gustavo <gustavogutin4@gmail.com>
2025-04-27 22:23:55 -0300

    Primeiro commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header () {
+    return (
+        <header>
+            <h1>Bem vindo ao meu site!</h1>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent () {
+    return (
+        <main>
+            <p>Este é o conteudo principal da pagina!</p>
+        </main>
+    );
+}
+
+export default MainContent;
